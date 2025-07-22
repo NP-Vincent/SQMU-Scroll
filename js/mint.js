ef50ebbdbb579b2a09428d14038aa7cecfbf2616
NP-Vincent <n.p.vincent@icloud.com>
2025-07-22 11:17:06 +0400

    Handle connect errors with red status

--- js/mint.js
+++ js/mint.js
@@ -27,6 +27,7 @@ async function connect() {
   statusDiv.innerHTML =
     '<span style="color:green;">Connected to Scroll. Contract ready!</span>';
   } catch (err) {
+    // Errors may occur if the ABI path is unreachable
     const statusDiv = document.getElementById('mint-status');
     statusDiv.innerHTML = `<span style="color:red;">${err.message}</span>`;
   }
