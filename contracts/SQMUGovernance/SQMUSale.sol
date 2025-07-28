fd04807a3cb4540786f9bcee33d9a68e4f2e189d
NP-Vincent <n.p.vincent@icloud.com>
2025-07-28 23:17:42 +0400

    Update SQMUSale.sol

--- contracts/SQMUGovernance/SQMUSale.sol
+++ contracts/SQMUGovernance/SQMUSale.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.26;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-import {SQMUVesting} from "./SQMUVesting.sol";
+import {SQMUVesting} from "https://np-vincent.github.io/SQMU-Scroll/contracts/SQMUGovernance/SQMUVesting.sol";
 
 /// @title SQMUSale
 /// @notice Handles payment collection and allocation of locked governance tokens.
